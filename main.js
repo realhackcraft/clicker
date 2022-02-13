963d193cad7672857f3df4289677e22e182b6963
Hackcraft_ <65378120+realhackcraft@users.noreply.github.com>
2022-02-13 10:18:28 -0500

    READABILITY-999 automaticly get money brrrrr

--- main.js
+++ main.js
@@ -2,13 +2,21 @@ money = document.getElementById('money');
 money.innerHTML = 0;
 max = document.getElementById('max');
 max.innerHTML = "Max money: $200";
+autoprinter = document.getElementById('autoprinter');
+automoney = document.getElementById('automoney');
+megaautoprinter = document.getElementById('megaautoprinter');
+efficiencydisplay = document.getElementById('efficiency');
 
 balance = 0;
 balancedisplay = "$0";
 cap = 200;
 efficiency = 1;
+autoprintercount = 0;
+megaautoprintercount = 0;
+passiverev = 0;
 
 interval = 100;
+autointerval = 1000;
 
 function addMoney(amount) {
   balance += amount;
@@ -18,19 +26,51 @@ function addMoney(amount) {
 }
 
 function getNewCard(amount) {
-  if (balance >= 200) {
+  if (balance >= 200 * amount) {
+    balance -= 200 * amount;
     cap += 200 * amount;
-    balance -= 200;
-    update;
   } else {
-    alert('You Don\'t Have Enogh Money!');
+    return;
   }
 }
 
+function getAutoprinter(amount) {
+  if (balance >= 400 * amount) {
+    balance -= 400 * amount;
+    autoprintercount++;
+  } else {
+    return;
+  }
+}
+
+function getMegaAutoprinter(amount) {
+  if (balance >= 800 * amount) {
+    balance -= 800 * amount;
+    megaautoprintercount++;
+    megaautoprinter.innerHTML = megaautoprintercount;
+  } else {
+    return;
+  }
+}
+
+function autoAddMoney() {
+  balance += autoprintercount + (megaautoprintercount * 5);
+}
+
 function update() {
+  if (balance > cap) {
+    balance = cap;
+  }
   balancedisplay = "$" + balance;
   money.innerHTML = balancedisplay;
   max.innerHTML = "$" + cap;
+  autoprinter.innerHTML = autoprintercount;
+  megaautoprinter.innerHTML = megaautoprintercount;
+  passiverev = autoprintercount + (megaautoprintercount * 5);
+  automoney.innerHTML = passiverev + "/s";
+  efficiencydisplay.innerHTML = "+" + efficiency + " per click";
 }
 
+
 setInterval(update, interval); // automation later
+setInterval(autoAddMoney, autointerval);
